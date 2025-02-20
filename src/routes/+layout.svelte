b5d32693e7f2507fcc7dcb5e04e5fd67c4c4d3f8
Kasper Aamodt <kasper@aamodt.xyz>
2025-02-20 02:31:06 +0100

    meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,12 +7,29 @@
 </script>
 
 <head:svelte>
-	<title>Send env</title>
+	<title>Sendenv</title>
 	<meta
 		name="description"
-		content="Securely share sensitive info with Send env. End-to-end encrypted for sharing env vars, API keys, and secrets."
+		content="Securely share sensitive info with Sendenv. End-to-end encrypted for sharing env vars, API keys, and secrets."
 	/>
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="og:title" content="Sendenv" />
+	<meta
+		name="og:description"
+		content="Securely share sensitive info with Sendenv. End-to-end encrypted for sharing env vars, API keys, and secrets."
+	/>
+	<meta name="og:image" content="https://sendenv.dev/og.png" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="Sendenv" />
+	<meta
+		name="twitter:description"
+		content="Securely share sensitive info with Sendenv. End-to-end encrypted for sharing env vars, API keys, and secrets."
+	/>
+	<meta name="twitter:image" content="https://sendenv.dev/og.png" />
+	<meta name="twitter:image:alt" content="Sendenv logo" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://sendenv.dev" />
+	<meta property="og:site_name" content="Sendenv" />
 </head:svelte>
 
 <Header />
